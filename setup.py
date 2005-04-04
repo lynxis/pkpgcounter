8b0e77a0197268f009973ce7150e90b6cc82d563
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2005-04-04 11:33:50 +0000

    Added setup script
    git-svn-id: http://svn.pykota.com/pkpgcounter/trunk@198 6164e503-883a-4105-99cd-1bc75b88fced

---
+++ setup.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+# -*- coding: ISO-8859-15 -*-
+#
+# pkpgcounter : a generic Page Description Language parser
+#
+# (c) 2003, 2004, 2005 Jerome Alet <alet@librelogiciel.com>
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+# $Id$
+#
+#
+
+import sys
+import glob
+import os
+import shutil
+try :
+    from distutils.core import setup
+except ImportError, msg :    
+    sys.stderr.write("%s\n" % msg)
+    sys.stderr.write("You need the DistUtils Python module.\nunder Debian, you may have to install the python-dev package.\nOf course, YMMV.\n")
+    sys.exit(-1)
+
+sys.path.insert(0, "pdlanalyzer")
+from pdlanalyzer.version import __version__, __doc__
+
+data_files = []
+mofiles = glob.glob(os.sep.join(["po", "*", "*.mo"]))
+for mofile in mofiles :
+    lang = mofile.split(os.sep)[1]
+    directory = os.sep.join(["share", "locale", lang, "LC_MESSAGES"])
+    data_files.append((directory, [ mofile ]))
+    
+docdir = "share/doc/pkpgcounter"    
+docfiles = ["README", "FAQ", "SECURITY", "COPYING", "LICENSE", "CREDITS", "TODO", "NEWS"]
+data_files.append((docdir, docfiles))
+
+directory = os.sep.join(["share", "man", "man1"])
+manpages = glob.glob(os.sep.join(["man", "*.1"]))    
+data_files.append((directory, manpages))
+
+setup(name = "pkpgcounter", version = __version__,
+      license = "GNU GPL",
+      description = __doc__,
+      author = "Jerome Alet",
+      author_email = "alet@librelogiciel.com",
+      url = "http://www.librelogiciel.com/software/",
+      packages = [ "pdlanalyzer" ],
+      scripts = [ "bin/pkpgcounter" ],
+      data_files = data_files)
