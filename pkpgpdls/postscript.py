cc4c54ea4972fac5732cb24f444ef8a144314679
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2006-02-07 09:36:13 +0000

    Does not launch ghostscript in the case the page description language used is KPDL (Kyocera Mita "enhanced" PostScript) : it is not needed since the internal parser is OK.
    git-svn-id: http://svn.pykota.com/pkpgcounter/trunk@308 6164e503-883a-4105-99cd-1bc75b88fced

--- pkpgpdls/postscript.py
+++ pkpgpdls/postscript.py
@@ -84,9 +84,12 @@ class Parser(pdlparser.PDLParser) :
         oldpagenum = None
         previousline = ""
         notrust = 0
+        kpdl = 0 # Kyocera Mita
         for line in self.infile.xreadlines() : 
-            if line.startswith(r"%%BeginResource: procset pdf") :
+            if (not kpdl) and line.startswith(r"%%BeginResource: procset pdf") :
                 notrust = 1 # Let this stuff be managed by GhostScript, but we still extract number of copies
+            elif line.startswith("/KPDLBASE") :
+                kpdl = 1
             elif line.startswith(r"%%Page: ") or line.startswith(r"(%%[Page: ") :
                 proceed = 1
                 try :
