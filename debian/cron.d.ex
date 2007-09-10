103d8331757428f6c17f1f0513d1360618c454ff
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2007-09-10 20:52:06 +0000

    Remove unnecessary files.
    git-svn-id: http://svn.pykota.com/pkpgcounter/trunk@465 6164e503-883a-4105-99cd-1bc75b88fced

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pkpgcounter package
-#
-0 4	* * *	root	pkpgcounter_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pkpgcounter):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="pkpgcounter" command="/usr/bin/pkpgcounter"
