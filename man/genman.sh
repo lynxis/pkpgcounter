267a74b9c0c908df0caf294c2f8d7e9e885a79ed
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2007-09-10 20:47:40 +0000

    Licensing terms changed to GNU GPL v3.0 or higher. Removed old PCL3/4/5 parser which for a long time now wasn't used anymore, and for which I was not the original copyright owner. Version number bumped to 3.00alpha to reflect licensing changes.
    git-svn-id: http://svn.pykota.com/pkpgcounter/trunk@463 6164e503-883a-4105-99cd-1bc75b88fced

--- man/genman.sh
+++ man/genman.sh
@@ -2,14 +2,19 @@
 #
 # pkpgcounter : a generic Page Description Language parser.
 #
-# (c) 2003, 2004, 2005, 2006, 2007 Jerome Alet <alet@librelogiciel.com>
-# You're welcome to redistribute this software under the
-# terms of the GNU General Public Licence version 2.0
-# or, at your option, any higher version.
-#
-# You can read the complete GNU GPL in the file COPYING
-# which should come along with this software, or visit
-# the Free Software Foundation's WEB site http://www.fsf.org
+# (c) 2003, 2004, 2005 Jerome Alet <alet@librelogiciel.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # $Id$
 #
