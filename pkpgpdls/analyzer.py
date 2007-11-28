ca983f39344565ad9ae602c62f46a197a15c8209
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2007-11-28 15:50:03 +0000

    Added helper function to later detect the presence of executable dependencies at runtime.
    git-svn-id: http://svn.pykota.com/pkpgcounter/trunk@524 6164e503-883a-4105-99cd-1bc75b88fced

--- pkpgpdls/analyzer.py
+++ pkpgpdls/analyzer.py
@@ -56,6 +56,14 @@ class PDLAnalyzer :
         self.workfile = None 
         self.mustclose = None
         
+    def findExecutable(self, command) :
+        """Finds an executable in the PATH and returns True if found else False."""
+        for path in os.environ.get("PATH", "").split(":") :
+            fullname = os.path.abspath(os.path.join(os.path.expanduser(path), command))
+            if os.path.isfile(fullname) and os.access(fullname, os.X_OK) :
+                return True
+        return False
+        
     def getJobSize(self) :    
         """Returns the job's size."""
         size = 0
