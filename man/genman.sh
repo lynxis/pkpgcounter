ceea40b01563fdf0736f1289fb32b3f98669e420
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2007-11-28 23:14:47 +0000

    Fixed the generation of the manual page to accomodate Debian packaging standard.
    git-svn-id: http://svn.pykota.com/pkpgcounter/trunk@531 6164e503-883a-4105-99cd-1bc75b88fced

--- man/genman.sh
+++ man/genman.sh
@@ -2,7 +2,7 @@
 #
 # pkpgcounter : a generic Page Description Language parser.
 #
-# (c) 2003, 2004, 2005 Jerome Alet <alet@librelogiciel.com>
+# (c) 2003, 2004, 2005, 2006, 2007 Jerome Alet <alet@librelogiciel.com>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -19,7 +19,15 @@
 # $Id$
 #
 for prog in pkpgcounter ; do 
-    echo $prog ;
-    help2man --no-info --section=1 --manual "User Commands" --source="C@LL - Conseil Internet & Logiciels Libres" --output=$prog.1 $prog ; 
+    echo "$prog" ;
+    help2man --no-info \
+             --section=1 \
+	     --name="count number of pages required to print various types of documents" \
+	     --manual="User Commands" \
+	     --source="C@LL - Conseil Internet & Logiciels Libres" \
+	     --output="temp$prog.1" \
+	     $prog ; 
+    /bin/sed -e "s/--/\\\-\\\-/g" <"temp$prog.1" >"$prog.1" ;
+    /bin/rm -f "temp$prog.1"
     echo ;
 done
